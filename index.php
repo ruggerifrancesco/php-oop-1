78913922f7986bc018887db4b6cddffc028a3a5d
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-13 17:01:07 +0200

    added a way to accept mutiple genres for a movie through an array

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
         public int $rating;
         public string $language;
         public string $poster;
+        public array $genres;
 
         public function __construct(
             string $name, 
             string $description, 
             int $rating, 
             string $language, 
-            string $poster
+            string $poster,
+            array $genres,
         ) {
             $this->name = $name;
             $this->description = $description;
             $this->rating = $rating;
             $this->language = $language;
             $this->poster = $poster;
+            $this->genres = $genres;
         }
 
         public function getDetails(): array {
@@ -26,10 +29,30 @@
                 'description' => $this->description,
                 'rating' => $this->rating,
                 'language' => $this->language,
-                'poster' => $this->poster
+                'poster' => $this->poster,
+                'genres' => $this->genres,
             ];
         }
     }
+
+    $genres = ["Drama", "Crime"];
+    $movie = new Movie(
+        "The Shawshank Redemption", 
+        "Two imprisoned men bond over several years, finding solace and eventual redemption through acts of common decency.", 
+        9, 
+        "English", 
+        "poster.jpg",
+        $genres);
+    $details = $movie->getDetails();
+
+    echo "Name: " . $details['name'] . "\n";
+    echo "Description: " . $details['description'] . "\n";
+    echo "Rating: " . $details['rating'] . "\n";
+    echo "Language: " . $details['language'] . "\n";
+    echo "Poster: " . $details['poster'] . "\n";
+
+    
+
 ?>
 
 <!DOCTYPE html>
