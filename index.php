cf42b625d8a97ae87b796f316a331e69cc160d69
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-13 15:49:48 +0200

    addedc new file fort style

--- index.php
+++ index.php
@@ -38,6 +38,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Oop</title>
+
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     
