86d50203c6c096c2fa7a1c556e5574a4bcaed40c
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-14 09:58:30 +0200

    chnaged handling of creating a new movie, created a new array list and foreach cycle all movies

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php 
     require_once __DIR__ . '/classes/movie.php';
 
-    $genres = ["Drama", "Crime"];
-    $movie = new Movie(
-        "The Shawshank Redemption", 
-        "Two imprisoned men bond over several years, finding solace and eventual redemption through acts of common decency.", 
-        9, 
-        "English", 
-        "poster.jpg",
-        $genres);
-    $details = $movie->getDetails();
-
-    echo "Name: " . $details['name'] . "\n";
-    echo "Description: " . $details['description'] . "\n";
-    echo "Rating: " . $details['rating'] . "\n";
-    echo "Language: " . $details['language'] . "\n";
-    echo "Poster: " . $details['poster'] . "\n";
-    echo "Genres: " . implode(", ", $details['genres']) . "\n";
-
+    $movielist = [
+        new Movie(
+            "The Shawshank Redemption",
+            "Two imprisoned men bond over several years, finding solace and eventual redemption through acts of common decency.",
+            9,
+            "English",
+            "poster.jpg",
+            ["Drama", "Crime"]
+        ),
+        new Movie(
+            "The Lord of the Rings: The Fellowship of the Ring",
+            "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+            8,
+            "English",
+            "poster.jpg",
+            ["Adventure", "Fantasy"]
+        )
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +31,19 @@
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    
+    <ul>
+        <?php foreach ($movielist as $movie) : ?>
+            <?php $details = $movie->getDetails(); ?>
+            <li>
+                <strong>Name:</strong> <?php echo $details['name']; ?><br>
+                <strong>Description:</strong> <?php echo $details['description']; ?><br>
+                <strong>Rating:</strong> <?php echo $details['rating']; ?><br>
+                <strong>Language:</strong> <?php echo $details['language']; ?><br>
+                <strong>Poster:</strong> <img src="<?php echo $details['poster']; ?>" alt="Movie Poster"><br>
+                <strong>Genres:</strong> <?php echo implode(", ", $details['genres']); ?><br>
+                <br>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
